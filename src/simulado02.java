0fa1c17a275c2e8b4cf031b640e8c79b0a29dc6d
CrisKoch <cristinak.iftm@gmail.com>
2020-05-22 21:25:42 -0300

    Leitura do nome e salario

--- src/simulado02.java
+++ src/simulado02.java
@@ -1,9 +1,37 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class simulado02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		//Leitura do nome e salario
+		
+		String nome;
+		double salario;
+		
+		System.out.println("DIGITE OS DADOS DO FINANCIAMENTO");
+		System.out.print("Nome do cliente: ");
+		
+		nome = sc.nextLine();
+		
+		
+		System.out.printf("Salario: ");
+		
+		salario = sc.nextDouble();
+		
+		while (salario <= 0) {
+			System.out.print("Valor inválido. Digite novamente");
+			salario = sc.nextDouble();
+		}
+		
+		
+		
+		
+		
+		sc.close();
 	}
 
 }
