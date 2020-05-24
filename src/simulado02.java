891ce740392ed5be2781d9ce962359872330d7a4
CrisKoch <cristinak.iftm@gmail.com>
2020-05-24 12:25:35 -0300

    Leitura do financiamento

--- src/simulado02.java
+++ src/simulado02.java
@@ -10,7 +10,8 @@ public class simulado02 {
 		//Leitura do nome e salario
 		
 		String nome;
-		double salario;
+		double salario, porcEntrada, totalFinanciado, valorPrestacao, prestPermitida;
+		int prestacoes;
 		
 		System.out.println("DIGITE OS DADOS DO FINANCIAMENTO");
 		System.out.print("Nome do cliente: ");
@@ -26,11 +27,28 @@ public class simulado02 {
 			System.out.print("Valor inválido. Digite novamente");
 			salario = sc.nextDouble();
 		}
+		//Leitura do financiamento
+		
+		do {
+			System.out.print("Numero de prestaçoes: ");
+				prestacoes = sc.nextInt();
+			System.out.print("Porcentagem de entrada: ");
+				porcEntrada = sc.nextDouble();
+			System.out.print("Valor total financiado: ");
+				totalFinanciado = sc.nextDouble();
+		
+		valorPrestacao = (totalFinanciado - (totalFinanciado * porcEntrada / 100)) / prestacoes;
+		prestPermitida = salario * 30 / 100;
+		if ( valorPrestacao > prestPermitida) {
+		System.out.println("O valor da prestação está acima do permitido. Diminua o valor total financiado, aumente a entrada ou o número de prestações");
+		}
+		}
+		while ( valorPrestacao > prestPermitida);
 		
 		
 		
-		
-		
+			
+			
 		sc.close();
 	}
 
